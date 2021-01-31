3ccb8280f959a07f43452fbaeb8deb9efd6388de
exlineo <gabbadie@step-sa.fr>
2021-01-31 19:16:46 +0100

    Mise à jour du site Web

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ProjetsComponent } from './projets/projets.component';
+import { PresentationComponent } from './presentation/presentation.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProjetsComponent,
+    PresentationComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([])
   ],
   providers: [],
   bootstrap: [AppComponent]
